c68fcce6bb58a4c10b47394edba85b871a9ff137
unknown <byron789_@hotmail.com>
2021-11-18 13:22:48 -0500

    Implementación métodos de cucumber

---
+++ src/test/java/co/com/choucair/certification/utest/stepdefinitions/ChoucairUtestStepDefinitions.java
@@ -0,0 +1,25 @@
+package co.com.choucair.certification.utest.stepdefinitions;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class ChoucairUtestStepDefinitions {
+
+
+    @Given("^than Byron wants to log into the Utest System$")
+    public void thanByronWantsToLogIntoTheUtestSystem() {
+
+    }
+
+    @When("^he needs to register in the Utest system$")
+    public void heNeedsToRegisterInTheUtestSystem() {
+
+    }
+
+    @Then("^he can enter the registration data$")
+    public void heCanEnterTheRegistrationData() {
+
+    }
+
+}
